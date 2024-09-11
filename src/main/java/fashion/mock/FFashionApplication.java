33d6b9f8cf69030fab037a52d9834a5713c1cabf
HuanNguyen01 <huannguyen199938@gmail.com>
2024-09-11 19:36:41 +0700

    .

---
+++ src/main/java/fashion/mock/FFashionApplication.java
@@ -0,0 +1,13 @@
+package fashion.mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FFashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FFashionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fashion/mock/FFashionApplicationTests.java
@@ -0,0 +1,13 @@
+package fashion.mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FFashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
